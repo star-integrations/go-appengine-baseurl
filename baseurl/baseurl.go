12252516ac19a5917e7deff9f1d8b587ad11954a
aska <aska@nl-plus.com>
2019-03-31 19:33:33 +0900

    Options pattern

--- baseurl/baseurl.go
+++ baseurl/baseurl.go
@@ -8,21 +8,68 @@ import (
 	"google.golang.org/appengine"
 )
 
-// NewBaseURL 接続先のAppEngineBaseURLを作成する
-func NewBaseURL(c context.Context, version, service, domain string) (*url.URL, error) {
+// BaseURL type
+type BaseURL struct {
+	version string
+	service string
+	domain  string
+}
+
+// Option type
+type Option func(*BaseURL) error
+
+// New Create AppEngine URL
+func New(c context.Context, options ...Option) (*url.URL, error) {
+	b := &BaseURL{}
+	for _, option := range options {
+		err := option(b)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	hostName := ""
-	if domain == "" {
+	if b.domain == "" {
 		hostName = appengine.DefaultVersionHostname(c)
 	} else {
-		hostName = domain
+		hostName = b.domain
 	}
-	if service != "" {
-		hostName = fmt.Sprintf("%s-dot-%s", service, hostName)
-	}
-	if version != "" {
-		hostName = fmt.Sprintf("%s-dot-%s", version, hostName)
-	}
-	hostName = fmt.Sprintf("https://%s", hostName)
 
-	return url.Parse(hostName)
+	if b.service != "" {
+		hostName = fmt.Sprintf("%s-dot-%s", b.service, hostName)
+	}
+
+	if b.version != "" {
+		hostName = fmt.Sprintf("%s-dot-%s", b.version, hostName)
+	}
+
+	baseURL := fmt.Sprintf("https://%s", hostName)
+
+	return url.Parse(baseURL)
+}
+
+// WithService function
+// See https://cloud.google.com/appengine/docs/standard/go/microservices-on-app-engine#service_isolation
+func WithService(s string) Option {
+	return func(b *BaseURL) error {
+		b.service = s
+		return nil
+	}
+}
+
+// WithVersion function
+// See https://cloud.google.com/appengine/docs/standard/go/microservices-on-app-engine#versions_within_services
+func WithVersion(v string) Option {
+	return func(b *BaseURL) error {
+		b.version = v
+		return nil
+	}
+}
+
+// WithDomain function
+func WithDomain(d string) Option {
+	return func(b *BaseURL) error {
+		b.domain = d
+		return nil
+	}
 }
--- baseurl/baseurl_test.go
+++ baseurl/baseurl_test.go
@@ -8,35 +8,35 @@ import (
 	"google.golang.org/appengine/aetest"
 )
 
-func TestNewBaseURL(t *testing.T) {
+func TestNew(t *testing.T) {
 	c, done, err := aetest.NewContext()
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer done()
 
-	// ドメインが空でデフォルトを設定
-	if url, err := NewBaseURL(c, "", "", ""); err != nil || url.String() != "https:" {
+	// Default
+	if url, err := New(c); err != nil || url.String() != "https:" {
 		t.Errorf("unmatch DefaultVersionHostname")
 	}
-	// デフォルトのサービスを設定
-	if url, err := NewBaseURL(c, "", "service", ""); err != nil || url.String() != fmt.Sprintf("https://service-dot-%s", appengine.DefaultVersionHostname(c)) {
+	// with Service
+	if url, err := New(c, WithService("service")); err != nil || url.String() != fmt.Sprintf("https://service-dot-%s", appengine.DefaultVersionHostname(c)) {
 		t.Errorf("unmatch service-DefaultVersionHostname")
 	}
-	// デフォルトのバージョンを設定
-	if url, err := NewBaseURL(c, "version", "service", ""); err != nil || url.String() != fmt.Sprintf("https://version-dot-service-dot-%s", appengine.DefaultVersionHostname(c)) {
+	// with Version
+	if url, err := New(c, WithService("service"), WithVersion("version")); err != nil || url.String() != fmt.Sprintf("https://version-dot-service-dot-%s", appengine.DefaultVersionHostname(c)) {
 		t.Errorf("unmatch service-DefaultVersionHostname")
 	}
-	// ドメインを設定
-	if url, err := NewBaseURL(c, "", "", "mydomain.com"); err != nil || url.String() != "https://mydomain.com" {
+	// with Domain
+	if url, err := New(c, WithDomain("mydomain.com")); err != nil || url.String() != "https://mydomain.com" {
 		t.Errorf("unmatch domain")
 	}
-	// サービスを設定
-	if url, err := NewBaseURL(c, "", "service", "mydomain.com"); err != nil || url.String() != "https://service-dot-mydomain.com" {
+	// with Domain and Service
+	if url, err := New(c, WithService("service"), WithDomain("mydomain.com")); err != nil || url.String() != "https://service-dot-mydomain.com" {
 		t.Errorf("unmatch service-domain")
 	}
-	// バージョンを設定
-	if url, err := NewBaseURL(c, "version", "service", "mydomain.com"); err != nil || url.String() != "https://version-dot-service-dot-mydomain.com" {
+	// with Domain and Service and Version
+	if url, err := New(c, WithVersion("version"), WithService("service"), WithDomain("mydomain.com")); err != nil || url.String() != "https://version-dot-service-dot-mydomain.com" {
 		t.Errorf("unmatch version-service-domain")
 	}
 }
